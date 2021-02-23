9569331f0074d4a6a39f590bc3d58be32ce136f7
Diogo Lopes <diogo.lopes@koerber.digital>
2021-02-23 19:16:58 +0000

    (feauture): Add connection to server

--- bot.py
+++ bot.py
@@ -8,12 +8,21 @@ from dotenv import load_dotenv
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
+#Get the server name
+GUILD = os.getenv('DISCORD_GUILD')
+
 #Sets up client
 client = discord.Client()
 
 @client.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
-    print("Successfully connected to server")
+    print(f'{client.user} has connected to Discord!\n')
+    
+    for i in client.guilds:
+        #Checks if the server name is the one we provided
+        if(i.name == GUILD):
+            break
+    
+    print(f'Connected to server {i.name}')
     
 client.run(TOKEN)
