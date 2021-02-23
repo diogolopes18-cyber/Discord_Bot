54053ab98766a73a48aecd8142f3bb475e38b4e9
Diogo Lopes <diogo.lopes@koerber.digital>
2021-02-23 18:50:01 +0000

    Add new print

--- bot.py
+++ bot.py
@@ -14,5 +14,6 @@ client = discord.Client()
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
+    print("Successfully connected to server")
     
 client.run(TOKEN)
