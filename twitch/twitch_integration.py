0aaadb2f15cfc8e1da207ccf87ce3e3bd945d853
diogolopes18-cyber <diogolopes18@ieee.org>
2021-07-29 16:12:31 +0100

    Remove Docker image since it is not necessary for the scope of the bot

--- twitch/twitch_integration.py
+++ twitch/twitch_integration.py
@@ -2,8 +2,23 @@
 
 import requests
 import json
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
+API_KEY = os.getenv('API_KEY')
 
 class twitchFetchStream():
+    
+    """
+    Checks if a stream is valid or if the streamer exists
+    """
     def __init__(self, streamer):
         self.apikey = api_key
+
+
+class embbedStream(twitchFetchStream):
+    
+    if(twitchFetchStream == True):
+        pass
+
