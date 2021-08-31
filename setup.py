665c2fc8d2dd7c74ba41116c86f27400574f4790
diogolopes18-cyber <diogolopes18@ieee.org>
2021-08-31 18:20:47 +0100

    Add setup for python package simplification

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+from setuptools import setup
+
+setup(
+    name='bot',
+    packages=['bot'],
+    install_requires=['python-dotenv == 0.15.0',
+                      'asyncio==3.4.3',
+                      'discord==1.7.3',
+                      'requests == 2.22.0',
+                      'wikipedia==1.4.0']
+)
