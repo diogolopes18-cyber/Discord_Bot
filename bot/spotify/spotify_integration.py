7de2c0b34ad1128ccdd03523f97edca4e9d37f42
diogolopes18-cyber <diogolopes18@ieee.org>
2021-09-07 10:09:10 +0100

    (feature): new Spotify methods, only the backbone

--- bot/spotify/spotify_integration.py
+++ bot/spotify/spotify_integration.py
@@ -5,6 +5,8 @@ import requests
 import json
 from dotenv import load_dotenv
 
+import spotify
+
 load_dotenv()
 CLIENT_ID = os.getenv('CLIENT_ID_SPOTIFY')
 
@@ -16,6 +18,25 @@ class authorizationFlow():
     def pkce_authorization(self):
         return -1
 
+class MediaPlayer():
+    def __init__(self):
+        self.client_id = CLIENT_ID
+        self.session = spotify.Session()
+
+    def check_for_link(self, playlist_url):
+        #Make connection to API
+        #Check if URL exists
+        #If it exists, check if it can play
+
+    def track_name(self):
+        '''
+        Checks the track name and returns the track name when audio is active
+        '''
+
+    def bitrate_correction(self):
+        '''
+        Fixes the bitrate while playing to adjust to the OS's needs
+        '''
 
 class spotifyReleases():
 
