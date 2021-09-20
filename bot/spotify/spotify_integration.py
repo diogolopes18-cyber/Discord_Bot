a70b53dc556c7851d5df743e59e304f1ddc88e65
unknown <luis.f.araujo.1997@gmail.com>
2021-09-20 01:18:39 +0100

    (new) add authorizationFlow method for spotify integration

--- bot/spotify/spotify_integration.py
+++ bot/spotify/spotify_integration.py
@@ -3,35 +3,62 @@
 import os
 from dotenv import load_dotenv
 
-import spotify
+from spotipy.oauth2 import SpotifyOAuth
+from spotipy import util
+import spotipy
+from discord.utils import get
+from discord import FFmpegPCMAudio
+
 
 load_dotenv()
-CLIENT_ID = os.getenv('CLIENT_ID_SPOTIFY')
+CLIENT_ID = os.getenv('CLIENT_ID')
+user=os.getenv('username')
+SECRET_CLIENT_ID=os.getenv('SECRET_CLIENT_ID')
+redirect_uri='http://localhost:8888/callback/'
 
 
-class authorizationFlow():
-    def __init__(self):
-        self.client_id_spotify = CLIENT_ID
-
-    def pkce_authorization(self):
-        return -1
 
 class MediaPlayer():
     def __init__(self):
-        self.client_id = CLIENT_ID
-        self.session = spotify.Session()
+        self.client_id_spotify = CLIENT_ID
+        self.client_secret_id = SECRET_CLIENT_ID
+        self.redirect_uri=redirect_uri
+        self.scope="user-read-recently-played user-library-read user-read-playback-state,user-modify-playback-state"
+        self.sp=None
 
-    def check_for_link(self, playlist_url):
+        self.auth_manager=SpotifyOAuth(client_id=self.client_id_spotify, client_secret= self.client_secret_id, redirect_uri=self.redirect_uri)
+        self.token_info=self.auth_manager.get_cached_token()
+        #self.authorizationFlow()
+
+    def authorizationFlow(self):
+        if(self.auth_manager.is_token_expired(self.token_info)):
+            self.token_info = self.auth_manager.refresh_access_token(self.token_info['refresh_token'])
+            token = self.token_info['access_token']
+            self.sp = spotipy.Spotify(auth=token)
+
+        self.sp=spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=self.client_id_spotify, client_secret= self.client_secret_id, redirect_uri=self.redirect_uri))
+
+
+
+    def check_for_link(self):
+        pass
         #Make connection to API
         #Check if URL exists
         #If it exists, check if it can play
 
     def track_name(self):
+        
+        pass
         '''
         Checks the track name and returns the track name when audio is active
         '''
 
     def bitrate_correction(self):
+        pass
         '''
         Fixes the bitrate while playing to adjust to the OS's needs
         '''
+
+
+
+
