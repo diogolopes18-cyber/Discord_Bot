633df857a6417420f757560696de36930528ce85
diogolopes18-cyber <diogolopes18@ieee.org>
2022-01-08 19:45:05 +0000

    feat: search for a specific Twitch channel

---
+++ bot/twitch/search_channel.py
@@ -0,0 +1,43 @@
+import requests
+import json
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_KEY = os.getenv("CLIENT_SECRET")
+CLIENT_ID = os.getenv("CLIENT_ID")
+URL = os.getenv("API_URL")
+
+class SearchChannel:
+    def __init__(self, channel_name):
+        self.key = API_KEY
+        self.client_id = CLIENT_ID
+        self.url = URL
+        self.channel = channel_name
+
+    def build_params(self):
+        params = {
+            "query": self.channel
+        }
+
+        return params
+
+    def build_header(self):
+        headers = {
+            "Authorization": self.key,
+            "Client-Id": self.client_id
+        }
+
+        return headers
+
+    def channel_search(self) -> bool:
+        '''
+        Searches for channels that match the specified name
+        '''
+
+        request = requests.get(self.url,
+                               params=self.build_params(),
+                               auth=self.build_header()).json()
+        
+        return request
