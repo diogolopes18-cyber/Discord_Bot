8b635b2ee01ada4a30023f31b0f1888c0b38ce34
diogolopes18-cyber <diogolopes18@ieee.org>
2022-01-17 22:57:44 +0000

    feat: add dictionary key for new url

--- bot/twitter/urls.py
+++ bot/twitter/urls.py
@@ -5,10 +5,14 @@ class EndpointUrls:
 
     def get_endpoint(self):
         endpoints = {
-            "search_user": f'https://api.twitter.com/2/users/by/username/{self.username}'
+            "search_user": f'https://api.twitter.com/2/users/by/username/{self.username}',
+            "tweet_by_topic": "https://api.twitter.com/2/tweets/search/recent"
         }
 
         if self.endpoint == "search":
             return endpoints["search_user"]
+
+        if self.endpoint == "tweet_by_topic":
+            return endpoints["tweet_by_topic"]
 
         return "Endpoint not found"
